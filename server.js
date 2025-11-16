8def823c823a3b8938aecccbd1f39ac699490c98
SharanyaGar <sgaripal@charlotte.edu>
2025-11-15 22:43:37 -0500

    second

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.use('/', express.static('public')); 
+
+app.get('/hello', (req, res) => {
+    res.send('Hi World');
+});
+
+const budget = require('./pBudget.json')
+//console.log(budget)
+
+
+
+
+app.get('/budget', (req, res) => {
+    console.log("fetching budget...")
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+});
